3c93b95760019b9fe8327da613025979d60508ef
Karol Prus <karolbosiak004@gmail.com>
2020-12-28 00:19:04 +0100

    updated sql create db file

--- db.sql
+++ db.sql
@@ -4,15 +4,17 @@ USE `shop_gen`;
 
 CREATE TABLE `user`(
     `user_id` int NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    `name` varchar(40) NOT NULL ,
     `email` varchar(40) NOT NULL UNIQUE ,
-    `password` binary(60) NOT NULL ,
+    `password` varchar(60) NOT NULL ,
     `active` boolean NOT NULL DEFAULT true,
     `registration_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE `shop`(
-    `shop_id` int NOT NULL PRIMARY KEY ,
+    `shop_id` int NOT NULL AUTO_INCREMENT PRIMARY KEY ,
     `user_id` int NOT NULL ,
+    `uid` varchar(50) NOT NULL ,
     `name` varchar(50) NOT NULL ,
     `options` json ,
     `shop_url` varchar(80) ,
@@ -20,3 +22,12 @@ CREATE TABLE `shop`(
 );
 
 ALTER TABLE `shop` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+
+INSERT INTO shop (user_id, uid, name, shop_url) values (1, 'shop1-uwvb37vbugvibfguuigfgu', 'shop1', 'https://shop1-uwvb37vbugvibfguuigfgu.heroku.com');
+INSERT INTO shop (user_id, uid, name, shop_url) values (1, 'shop2-uwvb37vbugvibfguuigfgu', 'shop2', 'https://shop2-uwvb37vbugvibfguuigfgu.heroku.com');
+INSERT INTO shop (user_id, uid, name, shop_url) values (1, 'shop3-uwvb37vbugvibfguuigfgu', 'shop3', 'https://shop3-uwvb37vbugvibfguuigfgu.heroku.com');
+
+SELECT * from user;
+
+SELECT * FROM shop;
--- front/pages/generate/index.vue
+++ front/pages/generate/index.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="container">
     <div id="controls" class="controls-wrapper">
-
+      <div id="myProgress">
+        <div id="myBar">10%</div>
+      </div>
     </div>
     <div id="setup" class="setup-wrapper">
-      <div class="get-started">
+      <div class="setup-item get-started">
         <button @click="start">Start</button>
       </div>
-      <div class="app-name">
+      <div class="setup-item app-name">
+        <input type="text">
+      </div>
+      <div class="setup-item shop-logo">
+        <input type="text">
+      </div>
+      <div class="setup-item template">
+        <input type="text">
+      </div>
+      <div class="setup-item categories">
+        <input type="text">
+      </div>
+      <div class="setup-item products">
         <input type="text">
       </div>
     </div>
@@ -22,22 +36,26 @@ export default {
     }
   },
   methods: {
+    move_bar(){
+      var elem = document.getElementById("myBar");
+      var width = 100;
+      elem.style.width = width + "%";
+      elem.innerHTML = width + "%";
+    },
     start(){
       var setup = document.getElementById("setup");
       var controls = document.getElementById("controls");
       console.log(setup.children);
       controls.style.display = 'block';
-      controls.style.animation = 'fading 2s infinite';
+      setup.children[0].style.display = 'none';
+      setup.children[1].style.display = 'block';
+      this.move_bar();
     }
   }
 }
 </script>
 
 <style scoped>
-@keyframes fading {
-  0% { opacity: 1; }
-  100% { opacity: 0; }
-}
 
 .container{
   height: calc(100vh - 80px);
@@ -52,11 +70,34 @@ export default {
   height: 75%;
 }
 .controls-wrapper{
-  display: block;
+  display: none;
   border-radius: 15px;
   box-shadow: 2px 3px 8px lightgray;
   width: 25%;
   height: 75%;
+}
+
+.app-name{
+  display: none;
+}
+
+.setup-item{
+  width: 100%;
+  height: 100%;
+}
+
+#myProgress{
+  margin: 10px;
+}
+
+#myBar {
+  border-radius: 5px;
+  width: 10%;
+  height: 30px;
+  background-color: #4CAF50;
+  text-align: center;
+  line-height: 30px;
+  color: white;
 }
 
 </style>
